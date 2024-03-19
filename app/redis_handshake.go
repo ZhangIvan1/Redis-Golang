92b659ccd6edbffe0c69565dc60cdb2623e0a3a6
ZhangIvan1 <zhang_ivan1@163.com>
2024-03-19 16:35:19 +0000

    Send handshake (2/3)

--- app/redis_handshake.go
+++ app/redis_handshake.go
@@ -7,8 +7,19 @@ import (
 	"time"
 )
 
-func (rd *Redis) sendHandshake(conn net.Conn) {
+func (rd *Redis) sendPing(conn net.Conn) {
 	if _, err := conn.Write([]byte("*1\r\n$4\r\nping\r\n")); err != nil {
+		fmt.Println("Error occur during handshaking to master:", err.Error())
+		return
+	}
+}
+
+func (rd *Redis) sendReplConf(conn net.Conn) {
+	if _, err := conn.Write([]byte("*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n" + rd.masterPort + "\r\n")); err != nil {
+		fmt.Println("Error occur during handshaking to master:", err.Error())
+		return
+	}
+	if _, err := conn.Write([]byte("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n")); err != nil {
 		fmt.Println("Error occur during handshaking to master:", err.Error())
 		return
 	}
@@ -22,8 +33,10 @@ func (rd *Redis) handshakeTicker() {
 			os.Exit(1)
 		}
 
-		go rd.sendHandshake(conn)
+		go rd.sendPing(conn)
 
-		time.Sleep(20 * time.Millisecond)
+		go rd.sendReplConf(conn)
+
+		go time.Sleep(20 * time.Millisecond)
 	}
 }
