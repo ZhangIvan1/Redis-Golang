51cf39815e2f75de95365133de402edad1f41611
ZhangIvan1 <zhang_ivan1@163.com>
2024-03-19 11:46:26 +0000

    Initial Replication ID and Offset

--- app/redis_info.go
+++ app/redis_info.go
@@ -2,10 +2,25 @@ package main
 
 import "strconv"
 
+const (
+	role               string = "role"
+	master_replid      string = "masterReplId"
+	master_repl_offset string = "masterReplOffset"
+)
+
 func (rd *Redis) info() (string, error) {
 	//info := "# Replication\r\n"
-	role := "role:" + rd.role
-	info := "$" + strconv.Itoa(len(role)) + "\r\n" + role + "\r\n"
+	info := ""
+
+	appendInfo(&info, role, rd.role)
+	appendInfo(&info, master_replid, rd.masterReplId)
+	appendInfo(&info, master_repl_offset, rd.masterReplOffset)
 
 	return info, nil
+}
+
+func appendInfo(info *string, key, value string) {
+	item := key + ":" + value
+	newInfo := *info + "$" + strconv.Itoa(len(item)) + "\r\n" + item + "\r\n"
+	*info = newInfo
 }
