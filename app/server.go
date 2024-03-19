366265a56628bf29c2cdaf08a658dee72758db60
ZhangIvan1 <zhang_ivan1@163.com>
2024-03-19 11:15:15 +0000

    The INFO command

--- app/server.go
+++ app/server.go
@@ -37,7 +37,7 @@ type Config struct {
 }
 
 func (config Config) String() string {
-	return fmt.Sprintf("config: {netType: %s, host: %s, port: %s, masterHost: %s, masterPort: %s}",
+	return fmt.Sprintf("{netType: %s, host: %s, port: %s, masterHost: %s, masterPort: %s}",
 		config.netType, config.host, config.port, config.masterHost, config.masterPort)
 }
 
@@ -79,7 +79,7 @@ func main() {
 		}
 	}
 
-	fmt.Println("config", config)
+	fmt.Println("config:", config)
 
 	rd := Make(config)
 
